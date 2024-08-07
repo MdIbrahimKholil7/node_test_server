e3d141660f384eb5df6c3ea3a4b8011320d3929e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2024-08-08 02:24:32 +0600

    change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 app.get("/", (req, res) => {
-    res.json({ message: "Success" })
+    res.json({ message: "Message from server" })
 })
 app.listen(5000, () => { console.log("Server listening on port 5000") })
