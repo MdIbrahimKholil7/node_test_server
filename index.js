ebe5cb5077a68d8e44e61302fc7d057e1180c942
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2024-10-13 00:13:38 +0600

    add

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 app.get("/", (req, res) => {
-    res.json({ message: "Message from server" })
-})
-app.listen(5000, () => { console.log("Server listening on port 5000") })
+  res.json({ message: "Message from server" });
+});
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server listening on port 5000");
+});
