21161009b173318c95374cef806fc2d148983187
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2024-10-13 15:23:23 +0600

    single product route modify

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.get("/api/health", (req, res) => {
   res.json({ message: "Message from server" });
 });
 app.get("/api/product", (req, res) => {
-  res.json({ message: "Product get success", data: [] });
+  res.json({ message: "Products get success", data: [] });
+});
+app.get("/api/product/:id", (req, res) => {
+  res.json({ message: "Single product get success", data: {} });
 });
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server listening on port 5000");
