e832885c16e9744f1caffe7e2a4d4d81ee38db94
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2024-10-13 15:21:43 +0600

    route edit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.json({ message: "Message from server" });
 });
+app.get("/api/product", (req, res) => {
+  res.json({ message: "Product get success", data: [] });
+});
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server listening on port 5000");
 });
